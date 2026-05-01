8685380d123ccef3abe3ba2891f08cb90c3575fe
Knayz <pesterev.andruha@gmail.com>
2026-05-01 22:56:58 +0300

    Add first functionallity

---
+++ src/CurlyBraceChecker.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Knayz\CurlyBraceChecker;
+
+class Checker {
+
+    public function check(string $str): bool
+    {
+        $str = preg_replace('/\s+/', '', $str);
+
+        if (strlen($str) % 2 === 1) return false;
+
+        $stack = new \SplStack();
+
+        $chars = str_split($str);
+
+        foreach ($chars as $char) {
+            if ($char === '(') {
+                $stack->push(true);
+            } else if ($char === ')') {
+                if ($stack->count() === 0) {
+                    return false;
+                }
+                $stack->pop();
+            }
+        }
+        return true;
+    }
+}
